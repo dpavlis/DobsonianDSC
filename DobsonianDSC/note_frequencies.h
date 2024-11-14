21426f50623e06cf4a4306b85bbb9288df0067c5
David Pavlis <david.pavlis@centrum.cz>
2024-11-14 21:43:13 +0100

    added handling of push-button and sound; extended config by adding initial alt&az position options

---
+++ DobsonianDSC/note_frequencies.h
@@ -0,0 +1,20 @@
+#ifndef note_frequencies
+#define NOTE_FREQUENCIES
+
+#define NOTE_FINISH 999
+#define NOTE_0 0
+#define NOTE_A 440
+#define NOTE_B_ 466
+#define NOTE_B 494
+#define NOTE_C 523
+#define NOTE_CS 554 //C sharp
+#define NOTE_D 587
+#define NOTE_DS 622
+#define NOTE_E 659
+#define NOTE_F 698
+#define NOTE_FS 740
+#define NOTE_G 784
+#define NOTE_A_ 831
+#define NOTE_A2 880
+
+#endif
